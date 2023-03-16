88d021f86f4c6b47b5d9fea6d2790880e23cb772
Giulia Carmen Dai <giuliadai1508@gmail.com>
2023-03-16 16:44:46 +0100

    Esercizio base + BONUS 1

---
+++ js/main.js
@@ -0,0 +1,54 @@
+// todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all’interno di una lista HTML un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        newTask:"",
+        todoList:[
+          {
+            text:'Fare la spesa',
+            done: true
+          },
+          {
+            text:'studiare',
+            done: true
+          },
+          {
+            text:'andare dalla nonna',
+            done: false
+          },
+        ]
+      }
+    },
+    methods:{
+
+      removeTask(index){
+        this.todoList.splice(index,1);
+      },
+
+      addTask(){
+        if(this.newTask.length > 0){
+          this.todoList.unshift({text:this.newTask,done:false});
+          this.newTask="";
+        }
+      },
+
+      taskDone(task){
+        if(task.done==false){
+          task.done = true;
+        }else{
+          task.done = false;
+        }
+      }
+    }
+  }).mount('#app')
